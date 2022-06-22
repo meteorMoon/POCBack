c65aa08fd38fb799210199d4b08815d27fd838d5
nicolas.echeverry <necheverry@unbosque.edu.co>
2022-06-22 18:43:02 -0500

    clear code with eslint

--- app/src/routes/index.ts
+++ app/src/routes/index.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 
+import { getUsers, getUserById, createUser, deleteUser, updateUserById, loginUser } from "../controllers/index.controller";
+import { TokenValidation } from "../libs/validateToken";
+
 const router = Router();
 
-import {getUsers,getUserById,createUser,deleteUser,updateUserById,loginUser} from '../controllers/index.controller';
-import {TokenValidation} from '../libs/validateToken';
-
-router.get('/users',getUsers)
-router.post('/login',loginUser)
-router.get('/users/:id',TokenValidation,getUserById)
-router.post('/users',createUser)
-router.put('/users/:id',updateUserById)
-router.delete('/users/:id',deleteUser)
+router.get("/users", getUsers);
+router.post("/login", loginUser);
+router.get("/users/:id", TokenValidation, getUserById);
+router.post("/users", createUser);
+router.put("/users/:id", updateUserById);
+router.delete("/users/:id", deleteUser);
 
 export default router;
--- postgres/users.sql
+++ postgres/users.sql
@@ -1,7 +1,49 @@
-create table users(
-    id serial primary key,
-    uuid varchar(36) not null,
-    name varchar(50),
-    password text,
-    email text
+
+CREATE TABLE users (
+	id_user serial PRIMARY KEY,
+	email VARCHAR ( 50 )  NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	name VARCHAR ( 255 )  NOT NULL,
+	last_name VARCHAR ( 255 )  NOT NULL,
+	created_on TIMESTAMP NOT NULL DEFAULT now(),
+    last_login TIMESTAMP,
+	fk_roles int NOT NULL,
+	fk_organizations int NOT NULL,
+    status CHAR(1) 	NOT NULL,
+	CONSTRAINT fk_roles
+      FOREIGN KEY(fk_roles) 
+	 REFERENCES roles(id_role),
+	CONSTRAINT fk_organizations
+      FOREIGN KEY(fk_organizations) 
+	 REFERENCES organization(id_organization)
+);
+
+CREATE TABLE organization (
+	id_organization serial PRIMARY KEY,
+	name_organization VARCHAR ( 255 )  NOT NULL,
+    status CHAR(1) 	NOT NULL
+);
+
+CREATE TABLE roles (
+	id_role serial PRIMARY KEY,
+	name_role VARCHAR ( 255 )  NOT NULL,
+    status CHAR(1) 	NOT NULL
+);
+
+CREATE TABLE modules_roles (
+	id_role int PRIMARY KEY,
+	id_module int PRIMARY KEY,
+	access VARCHAR ( 255 )  NOT NULL,
+    status CHAR(1) 	NOT NULL,
+	CONSTRAINT fk_role
+      FOREIGN KEY(id_role) 
+	 REFERENCES roles(id_role),
+	CONSTRAINT fk_module
+      FOREIGN KEY(id_module) 
+	 REFERENCES modules(id_module)
+);
+CREATE TABLE modules (
+	id_module serial PRIMARY KEY,
+	name_module VARCHAR ( 255 )  NOT NULL,
+    status CHAR(1) 	NOT NULL
 );
