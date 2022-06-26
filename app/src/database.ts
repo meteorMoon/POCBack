5cd99dbaa60c8657956d003c61e84866a1433bfa
esteban <valarmelkor2@gmail.com>
2022-06-25 23:52:58 -0500

    docker funcionando

--- app/src/database.ts
+++ app/src/database.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg'
 
 export const pool=new Pool({
     user: 'postgres',
-    host: 'localhost',
+    host: 'postgres',
     database: 'typescriptdatabase',
     password: 'mysecretpassword',
     port: 5432,
